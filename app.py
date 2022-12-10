6f72c36c22f5c56814625b7523d24779382282bd
Waina <wainaze@gmail.com>
2022-12-10 14:08:49 +0000

    change dataframe to look nicer on webpage

--- app.py
+++ app.py
@@ -50,8 +50,10 @@ df['transp_link'] = df['WEBSITE'].map(transp_url)
 df = df.astype({'transp_link':'string'})
 pd.set_option('display.max_colwidth', None)
 #df = df.style.format({'cbb_link': make_clickable})
+df = df.rename(columns={'NOM ENREGISTRE A LA BANQUE NATIONALE	': 'NOM', 'cbb_link': 'Dernière publication', 'transp_link': 'Page transparence' })
 
-df_short= df.drop(['WEBSITE', 'bce_link', 'Autorite', 'TYPE', 'MANDATS', 'UNITES'], axis=1)
+
+df_short= df.drop(['NUM','WEBSITE', 'bce_link', 'Autorite', 'TYPE', 'MANDATS', 'UNITES'], axis=1)
 
 #print (df)
 
